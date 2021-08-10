12b79dbe8e3b9dd01affd32a52fcbdd5cfae6cca
fane-eternal <71985710+fane-eternal@users.noreply.github.com>
2021-08-10 08:52:58 -0400

    qwe

--- DKA Hackathon/main.py
+++ DKA Hackathon/main.py
@@ -9,11 +9,7 @@ import matplotlib.pyplot as plt
 #import seaborn library (wrapper of matplotlib)
 import seaborn as sns
 
-df = pd.read_csv('https://raw.githubusercontent.com/fane-eternal/test-csv/main/Database_sample.csv', sep=r'\s*,\s*',
-header=0, error_bad_lines=False, engine='python')
+df = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/main/data/countries-aggregated.csv', parse_dates = ['Date'])
+df['Total Cases'] = df[['Confirmed', 'Recovered', 'Deaths']].sum(axis = 1)
 print(df.head())
 print(df.info())
-rating = df.loc[:, 'rating'].values
-gross_revenue = df.loc[:, 'Gross Revenue'].values
-duration = df.loc[:, 'Duration (min)'].values
-plt.plot(rating, duration)
